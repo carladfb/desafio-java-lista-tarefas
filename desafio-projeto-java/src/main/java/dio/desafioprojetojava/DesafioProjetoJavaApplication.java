11b2dd9795566d4de976f01a2710e6c520d9e926
Carla <107011235+carladfb@users.noreply.github.com>
2023-10-17 15:34:15 -0300

    Add files via upload

---
+++ desafio-projeto-java/src/main/java/dio/desafioprojetojava/DesafioProjetoJavaApplication.java
@@ -0,0 +1,13 @@
+package dio.desafioprojetojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoJavaApplication.class, args);
+	}
+
+}
