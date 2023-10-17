f6030b3fad01b61205c813f8fedab88302147840
Carla <107011235+carladfb@users.noreply.github.com>
2023-10-17 15:49:39 -0300

    Add files via upload

---
+++ desafio-projeto-java/src/test/java/dio/desafioprojetojava/DesafioProjetoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafioprojetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
